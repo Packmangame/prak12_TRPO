1246f17f74099f23f809ab9b0c473cca34b820d1
xckrt <xckrt@github.com>
2025-11-25 10:02:11 +0500

    1

---
+++ Classes/User.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak12.Classes
+{
+    public class User:INotifyPropertyChanged
+    {
+        long _id;
+        string _login;
+        string _name;
+        string _email;
+        string _password;
+        DateTime _createdAt;
+
+        public long ID
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                if (_login != value)
+                {
+                    _login = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Password
+        { get => _password;
+            set
+            {
+                if (value != _password)
+                {
+                    _password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime CreatedAt
+        {
+            get => _createdAt;
+            set
+            {
+                if (_createdAt != value)
+                    _createdAt = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ ValidationRules/PasswordRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace prak12.ValidationRules
+{
+    internal class PasswordRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var input = (value ?? "").ToString().Trim();
+
+            if (input.Length < 6)
+            {
+                return new ValidationResult(false, "Пароль должен содержать не менее 6 символов");
+            }
+            if (!input.Any(char.IsUpper))
+            {
+                return new ValidationResult(false, "Пароль должен содержать хотя бы одну заглавную букву");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
